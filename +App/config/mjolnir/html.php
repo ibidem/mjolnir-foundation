5f2c66a5829730382f089aae5c28c6d07e5cef4a
srcspider <source.spider@gmail.com>
2013-02-19 15:27:16 +0200

    viewport default

--- +App/config/mjolnir/html.php
+++ +App/config/mjolnir/html.php
@@ -4,5 +4,6 @@ $mjolnir_base = \app\CFS::config('mjolnir/base');
 
 return array
 	(
+		'viewport' => 'width=device-width, initial-scale=1',
 		'js-loader' => '//'.$mjolnir_base['domain'].$mjolnir_base['path'].'media/static/yepnope.latest-min.js',
 	);
