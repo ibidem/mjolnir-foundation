1821310563436719db11daab9fc2380be931bcf4
srcspider <source.spider@gmail.com>
2013-03-26 14:41:32 +0200

    fix'ed configuration

--- +App/config/mjolnir/html.php
+++ +App/config/mjolnir/html.php
@@ -5,5 +5,5 @@ $baseconfig = CFS::config('mjolnir/base');
 return array
 	(
 		'viewport' => 'width=device-width, initial-scale=1',
-		'js-loader' => '//'.$baseconfig['domain'].$baseconfig['path'].'media/static/yepnope.latest-min.js',
+		'js-loader' => '//'.$baseconfig['domain'].$baseconfig['path'].'media/include/yepnope.latest-min.js',
 	);
