e63286a3c90c97068be630908a6d0b1ac43ef0e5
srcspider <source.spider@gmail.com>
2013-03-26 11:29:29 +0200

    fix'ed error reporting

--- Controller/Error.php
+++ Controller/Error.php
@@ -34,26 +34,118 @@ class Controller_Error extends \app\Instantiatable implements \mjolnir\types\Con
 	 */
 	protected function handle_exception($exception, $classname)
 	{
-		// @todo more elegant exception reporting
-		
+		// bellow we don't need to check for command line usage since we are
+		// in a controller which is only likely to be called by another 
+		// controller as part of a http request
+
 		\mjolnir\log_exception($exception);
-		
+
 		switch ($classname)
 		{
 			case 'Exception_NotFound':
-				return 'not found';
+				try 
+				{
+					return \app\ThemeView::fortarget('exception-NotFound')
+						->pass('control', $this)
+						->pass('context', $this);
+				}
+				catch (\Exception $e)
+				{
+					\mjolnir\log_exception($e);
+
+					$wwwpath = \app\Env::key('www.path');
+					if ($wwwpath !== null && \file_exists($wwwpath.'404'.EXT))
+					{
+						include $wwwpath.'404'.EXT;
+						exit(1);
+					}
+
+					return 'Resource Not Found. Please verify the data provided.';
+				}
 
 			case 'Exception_NotAllowed':
-				return 'not allowed';
+				try 
+				{
+					return \app\ThemeView::fortarget('exception-NotAllowed')
+						->pass('control', $this)
+						->pass('context', $this);
+				}
+				catch (\Exception $e)
+				{
+					\mjolnir\log_exception($e);
+
+					$wwwpath = \app\Env::key('www.path');
+					if ($wwwpath !== null && \file_exists($wwwpath.'403'.EXT))
+					{
+						include $wwwpath.'403'.EXT;
+						exit(1);
+					}
+
+					return 'Operation Not Allowed. You require higher access privalages to perform the operation.';
+				}
 
 			case 'Exception_NotApplicable':
-				return 'not applicable';
+				try 
+				{
+					return \app\ThemeView::fortarget('exception-NotApplicable')
+						->pass('control', $this)
+						->pass('context', $this);
+				}
+				catch (\Exception $e)
+				{
+					\mjolnir\log_exception($e);
+
+					$wwwpath = \app\Env::key('www.path');
+					if ($wwwpath !== null && \file_exists($wwwpath.'500'.EXT))
+					{
+						include $wwwpath.'500'.EXT;
+						exit(1);
+					}
+
+					return 'Not Applicable: '.$exception->getMessage();
+				}
 
 			case 'Exception_NotImplemented':
-				return 'not implemented';
-				
+				try 
+				{
+					return \app\ThemeView::fortarget('exception-NotImplemented')
+						->pass('control', $this)
+						->pass('context', $this);
+				}
+				catch (\Exception $e)
+				{
+					\mjolnir\log_exception($e);
+
+					$wwwpath = \app\Env::key('www.path');
+					if ($wwwpath !== null && \file_exists($wwwpath.'500'.EXT))
+					{
+						include $wwwpath.'500'.EXT;
+						exit(1);
+					}
+
+					return 'Not Implemented';
+				}
+
 			default:
-				return 'unknown';
+				try 
+				{
+					return \app\ThemeView::fortarget('exception-Unknown')
+						->pass('control', $this)
+						->pass('context', $this);
+				}
+				catch (\Exception $e)
+				{
+					\mjolnir\log_exception($e);
+
+					$wwwpath = \app\Env::key('www.path');
+					if ($wwwpath !== null && \file_exists($wwwpath.'500'.EXT))
+					{
+						include $wwwpath.'500'.EXT;
+						exit(1);
+					}
+
+					return 'An unknown error has occured. We apologize for the inconvenience.';
+				}
 		}
 	}
 
